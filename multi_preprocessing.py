8d49edc990f45c58b674f5b26dd2a016d2b0ed1f
danibahnani <danielpadban@gmail.com>
2024-04-12 00:10:13 +0200

    initial sync with dev

--- multi_preprocessing.py
+++ multi_preprocessing.py
@@ -1,5 +1,6 @@
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from torch.utils.data import dataloader
+from numpy import dtype, float32
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from torch.utils.data import DataLoader,Dataset,TensorDataset
 import pandas as pd
 import torch
 import itertools
@@ -12,7 +13,7 @@ sw_df['date'] = pd.to_datetime(sw_df['date'])
 unix_epoch = pd.Timestamp('1970-01-01')
 
 unix_timestamps = (sw_df['date']-unix_epoch)//pd.Timedelta(seconds=1)
-sw_df['unix_timestamps'] = unix_timestamps
+sw_df['date'] = unix_timestamps
 print(sw_df.head())
 #print(sw_df.date.dtype)
 
@@ -25,7 +26,7 @@ y_data = pd.DataFrame(sw_df[y_col])
 x_q_cols = ['unix_timestamps','precipitation','temp_max','temp_min',]
 
 x_q_data = sw_df[x_q_cols]
-scaler = StandardScaler()
+scaler = MinMaxScaler((0,1))
 x_q_scaled_data = scaler.fit_transform(x_q_data)
 
 # x categorical columns (weather) - one hot encoding
@@ -37,6 +38,7 @@ OHE_df = pd.DataFrame(OHE_transform,columns=OHE_cols,index=sw_df.index)
 
 # Split data into train, val and test sets
 #Train data - start to row 731 = 2012-01-01 to 2013-12-31
+print(x_q_scaled_data.shape)
 x_q_train = x_q_scaled_data[:731]
 #x_c_train = OHE_df[:731]
 y_train = y_data.iloc[:731]
@@ -79,19 +81,46 @@ y_test_tensor = torch.tensor(y_test.values)
 
 #x_test_tensor = torch.concat([xq_test_tensor,xc_test_tensor])
 
-train_tensorset = [xq_train_tensor,y_train_tensor]
+
+
+xq_train_tensor = xq_test_tensor.float()
+xq_val_tensor  = xq_val_tensor.float()
+xq_test_tensor = xq_test_tensor.float()
+
+y_train_tensor = y_train_tensor.float()
+y_val_tensor = y_val_tensor.float()
+y_test_tensor = y_test_tensor.float()
+
+print(xq_train_tensor)
+
+print(xq_train_tensor.type())
+
+#print(f"Num tensors:{len(flat_tensor_list)}")
+
+#not working:
+'''train_tensorset = [xq_train_tensor,y_train_tensor]
 val_tensorset = [xq_val_tensor,y_val_tensor]
 test_tensorset = [xq_test_tensor,y_test_tensor]
 
 tensor_list = [train_tensorset,val_tensorset,test_tensorset]
 flat_tensor_list = list(itertools.chain.from_iterable(tensor_list))
-print(f"Num tensors:{len(flat_tensor_list)}")
 
-for i in range(0,len(flat_tensor_list)):
-    flat_tensor_list[i] = flat_tensor_list[i].float() 
-    print(f"{i}: {flat_tensor_list[i].type()}")
+def update_tensor_type(tensor_list,datatype):
+    for i, tensor in enumerate(flat_tensor_list):
+        conv_tensor_list = []
+        tensor = tensor_list[i].type(dtype=datatype)
+        conv_tensor_list.append(tensor)
+        print(f"{i}: {tensor.type()}")
+    return conv_tensor_list
+
+xq_train_tensor, y_train_tensor, xq_val_tensor, y_val_tensor, xq_test_tensor,y_test_tensor = update_tensor_type(tensor_list=flat_tensor_list,datatype=torch.float32)'''
+
+
+#credit datatype conversion: 
+'''https://stackoverflow.com/questions/78307251/how-to-convert-the-tensor-type-of-
+a-list-of-pytorch-tensors-using-a-for-loop?noredirect=1#comment138053539_78307251'''
 
 # ***** new *****
 # ----- Dataloader -----
-
-
+'''train_dataset = TensorDataset(xq_train_tensor,y_train_tensor)
+val_dataset = TensorDataset(xq_val_tensor,y_val_tensor)'''
--- sine_nn.py
+++ sine_nn.py
@@ -1,3 +1,4 @@
+import numpy
 import torch
 import pandas as pd
 import torch.nn as nn
@@ -7,11 +8,16 @@ from multi_preprocessing import\
     xq_test_tensor,y_test_tensor
 
 
+device = torch.device("cpu")
+
+
 class SinActivation(nn.Module):
+    #sine activation function
     def forward(self,input):
         return torch.sin(input)
 
 class SineNet(nn.Module):
+    #model architechture
     def __init__(self,input_size,hidden_size, stacked_layers, output_size):
         super(SineNet, self).__init__()
         self.hidden_size = hidden_size
@@ -20,6 +26,7 @@ class SineNet(nn.Module):
         self.lstm = nn.LSTM(input_size,hidden_size,stacked_layers,batch_first=True)
         self.fc1 = nn.Linear(hidden_size,output_size)
 
+    #feedforward structure
     def forward(self, x):
         lstm = self.lstm
         batch_size = x.size(0)
@@ -29,17 +36,31 @@ class SineNet(nn.Module):
 
         lstm_out, (hn,cn) = lstm(x, (h0,c0))
 
-        output = self.fc1(lstm_out[:,-1,:])
+        output = self.fc1(lstm_out[:,:])
 
         return output
 
-model =  SineNet(4,16,1,1)
-
+model =  SineNet(4,16,1,1).to(device)
 print(model)
+input_tensor = xq_train_tensor
 
-test_run = model(xq_train_tensor)
+print(f"Input tensor:\n{input_tensor}")
+print(input_tensor.shape)
 
-print(test_run)
 
-test_result = pd.DataFrame(test_run)
-test_result.to_csv('test_result.csv')
+lossfunc = nn.MSELoss()
+#lossfunc.backward()
+
+learning_rate = 1e-3
+batch_size = 73
+epochs = 5
+
+
+
+result_tensor = model(xq_train_tensor)
+#result = result_tensor.detach().numpy()
+print(f"Result tensor: {result_tensor}")
+print(result_tensor.shape)
+
+#test_result = pd.DataFrame(result)
+#test_result.to_csv('test_result.csv')
