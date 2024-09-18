18affd908614cea5a5253ec62f6da422dfbdd15d
danibahnani <danielpadban@gmail.com>
2024-09-18 13:22:16 +0200

    sin l2 activation

--- docker_reqs/ka_train.py
+++ docker_reqs/ka_train.py
@@ -20,7 +20,7 @@ device = (
 print(device)
 
 config_dict = json2dict('docker_reqs/config.json')
-run = wandb.init(project='Sine-Gates',config=config_dict,group='Ka-L2-tanh')
+run = wandb.init(project='Sine-Gates',config=config_dict,group='Ka-L2-sin')
 
 step_size = run.config['step_size']
 
