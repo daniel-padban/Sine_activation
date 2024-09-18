b0384f7f8a7e6776a629837dfe2b4da5e1e21426
danibahnani <danielpadban@gmail.com>
2024-09-19 01:35:49 +0200

    spaced out seeds

--- docker_reqs/run_loop.py
+++ docker_reqs/run_loop.py
@@ -4,5 +4,7 @@ n_runs=100
 import subprocess
 
 for i in range(n_runs):
-   subprocess.run(['python','docker_reqs/ka_train.py','--seed',str(i)])
+   current_seed = i*100 + i
+
+   subprocess.run(['python','docker_reqs/ka_train.py','--seed',str(current_seed)])
 
