d4048d7cc07e74d5ded514d80abd3b349c94ed3f
danibahnani <danielpadban@gmail.com>
2024-09-20 21:57:37 +0200

    remaining runs (75-100) sin l4

--- docker_reqs/run_loop.py
+++ docker_reqs/run_loop.py
@@ -1,9 +1,9 @@
 #!/bin/bash
 n_runs=100
-
+start = 75
 import subprocess
 
-for i in range(n_runs):
+for i in range(start,n_runs):
    current_seed = i*100 + i
 
    subprocess.run(['python','docker_reqs/ka_train.py','--seed',str(current_seed)])
